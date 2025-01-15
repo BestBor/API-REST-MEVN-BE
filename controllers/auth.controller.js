ddec63c10bfbed26b49ec1f90d631562cbd31236
BestBor <bbordaa@unal.edu.co>
2025-01-14 23:45:25 -0500

    Agregada prueba de express validator

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,13 @@
+import { validationResult } from "express-validator";
+
 export const register = (req, res) => {
-    res.json({ ok: true });
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    res.status(200).json({ message: "Usuario registrado con éxito" }); // dummie code
 }
 
 export const login = (req, res) => {
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,23 @@
 import express from "express";
 import { login, register } from "../controllers/auth.controller.js";
+import { body } from "express-validator";
 
 const router = express.Router();
 
 router.post("/login", login);
 
-router.post("/register", register);
+router.post("/register",
+    [
+        body("email", "Formato de email incorrecto")
+            .trim()
+            .isEmail()
+            .normalizeEmail(),
+        body("password", "Formato de password incorrecto")
+            .trim()
+            .isLength({ min: 6 })
+    ],
+    register
+);
 
 // app.get("/", (req, res) => {
 //     res.json({ok: true})
